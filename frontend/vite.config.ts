1bc8e918999ae527188a05eb534d46344f4ec875
hcstephencheung <hc.stephen.cheung@gmail.com>
2025-06-01 13:25:24 -0700

    run port on env variable

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   ],
   server: {
     host: '0.0.0.0',
-    port: 3000,
+    port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
     proxy: {
       '/api': {
         target: 'http://backend:9000', // Backend server
